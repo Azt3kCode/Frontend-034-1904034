8f5e4fb0c2381580816a4085d1e52229f258625c
Azt3kCode <62321351+Azt3kCode@users.noreply.github.com>
2024-04-24 21:11:24 -0600

    IMPORTAR Y EXPORTAR

---
+++ src/data/carros.js
@@ -0,0 +1,66 @@
+export const carros = [
+    {
+        marca: 'Seat',
+        modelo: 'Fr',
+        anio: 2018,
+        nombre: 'Leon',
+        client: {
+            name: 'Alexis',
+            apellido: 'Trejo'
+        }
+    },
+    {
+        marca: 'Toyota',
+        modelo: 'Corolla',
+        anio: 2019,
+        nombre: 'Sedán',
+        client: {
+            name: 'Zaira',
+            apellido: 'Gómez'
+        }
+    },
+    {
+        marca: 'Ford',
+        modelo: 'Fiesta',
+        anio: 2020,
+        nombre: 'Hatchback',
+        client: {
+            name: 'Renato',
+            apellido: 'López'
+        }
+    },
+    {
+        marca: 'Honda',
+        modelo: 'Civic',
+        anio: 2017,
+        nombre: 'Sedán',
+        client: {
+            name: 'Carolina',
+            apellido: 'Martínez'
+        }
+    },
+    {
+        marca: 'Volkswagen',
+        modelo: 'Golf',
+        anio: 2016,
+        nombre: 'Hatchback',
+        client: {
+            name: 'Eduardo',
+            apellido: 'Rodríguez'
+        }
+    },
+    {
+        marca: 'Chevrolet',
+        modelo: 'Camaro',
+        anio: 2021,
+        nombre: 'Deportivo',
+        client: {
+            name: 'Brenda',
+            apellido: 'Sánchez'
+        }
+    }
+];
+
+export const carroPorNombre = (nombreCliente) => {
+    return carros.find(i => i.client.name === nombreCliente);
+}
